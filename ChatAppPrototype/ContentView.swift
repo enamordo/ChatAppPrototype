58a1ef6ebf1e76676f26bcc01b39153557ffe779
AY <yugechan666@gmail.com>
2024-09-29 17:26:26 +0900

    タブビューでラフイメージを作成

--- ChatAppPrototype/ContentView.swift
+++ ChatAppPrototype/ContentView.swift
@@ -1,15 +1,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    // スペース追加
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView{
+            FirstPage()
+                .tabItem {
+                    Label("つぶやく", systemImage: "bubble.right")
+                }
+            SecondPage()
+                .tabItem {
+                    Label("やりとり", systemImage: "envelope.fill")
+                }
+            ThirdPage()
+                .tabItem {
+                    Label("タイムライン", systemImage: "bubble.left.and.bubble.right")
+                }
+            FourthPage()
+                .tabItem {
+                    Label("設定/ヘルプ", systemImage: "gearshape.2")
+                }
         }
-        .padding()
+    }
+}
+
+struct FirstPage: View {
+    var body: some View {
+        Text("1枚目")
+            .font(.title)
+            .foregroundColor(.green)
+    }
+}
+
+struct SecondPage: View {
+    var body: some View {
+        Text("2枚目")
+            .font(.title)
+            .foregroundColor(.green)
+    }
+}
+
+struct ThirdPage: View {
+    var body: some View {
+        Text("3枚目")
+            .font(.title)
+            .foregroundColor(.green)
+    }
+}
+
+struct FourthPage: View {
+    var body: some View {
+        Text("4枚目")
+            .font(.title)
+            .foregroundColor(.green)
     }
 }
 
